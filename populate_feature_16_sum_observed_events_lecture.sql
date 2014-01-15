dfba58be462285142686de9fdc34e470fd1b1d7a
colin <colin2328@gmail.com>
2014-01-15 17:59:05 -0500

    added correct resource_types

--- populate_feature_16_sum_observed_events_lecture.sql
+++ populate_feature_16_sum_observed_events_lecture.sql
@@ -1,7 +1,8 @@
 -- Takes 1500 seconds to execute
 -- Created on August 23, 2013
 -- @author: Franck for ALFA, MIT lab: franck.dernoncourt@gmail.com
--- Feature 16: Total time spent on all resources - courseware only - during the week
+-- Feature 16: Total time spent on all resources - lecture only - during the week
+-- edited by Colin Taylor to include correct resource_type
 
 INSERT INTO moocdb.dropout_feature_values(dropout_feature_id, user_id, dropout_feature_value_week, dropout_feature_value)
 	
@@ -19,7 +20,7 @@ INNER JOIN moocdb.resource_types AS resource_types
  ON resource_types.resource_type_id = resources.resource_type_id
 WHERE users.user_dropout_week IS NOT NULL 
 	-- AND users.user_id < 100
-	AND resource_types.resource_type_id = 1 -- 1 is courseware
+	AND resource_types.resource_type_id = 1 -- 1 is lecture
 	AND FLOOR((UNIX_TIMESTAMP(observed_events.observed_event_timestamp) 
 			- UNIX_TIMESTAMP('2012-03-05 12:00:00')) / (3600 * 24 * 7)) < 16
 GROUP BY users.user_id, week
