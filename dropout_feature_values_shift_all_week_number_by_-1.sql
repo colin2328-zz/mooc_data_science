f9832a5eca1ad53ee4bc22144b19471c30790bcf
colin <colin2328@gmail.com>
2014-01-30 14:09:23 -0500

    removed extra files

--- dropout_feature_values_shift_all_week_number_by_-1.sql
+++
@@ -1,8 +0,0 @@
--- Takes 60 seconds to execute (depends on how many features we have)
--- Created on July 2, 2013
--- @author: Franck for ALFA, MIT lab: franck.dernoncourt@gmail.com
--- Shift all week number by -1
-
-
-UPDATE moocdb.dropout_feature_values AS dropout_feature_values
-SET dropout_feature_values.dropout_feature_value_week = dropout_feature_values.dropout_feature_value_week - 1
