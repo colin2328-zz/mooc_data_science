1805c0fe1b8873c8a2caff9b1b06752d01df9645
colin <colin2328@gmail.com>
2014-04-07 17:44:17 -0400

    added logistic regression graphing and dropout_week analysis

---
+++ learning/generate_dropout_week.py
@@ -0,0 +1,26 @@
+import numpy as np
+import csv
+
+in_file = "features.csv"
+data = np.genfromtxt(in_file, delimiter = ',', skip_header = 1)
+
+out_file = "dropout_week.csv"
+headers = ["student_id", "dropout_week"]
+writer = csv.DictWriter(open(out_file, 'wb'), delimiter= ",", fieldnames=headers)
+writer.writeheader()
+
+start_idx = 0
+end_idx = len(data)
+num_weeks = 15
+
+values = {"student_id": 1}
+while start_idx < end_idx:	
+	dropouts = data[start_idx : start_idx + num_weeks, 1].tolist()
+	try:
+		values["dropout_week"] = dropouts.index(0) + 1
+	except ValueError:
+		values["dropout_week"] = 15
+	writer.writerow(values)
+
+	start_idx += num_weeks #move to next student
+	values["student_id"] +=1
---
+++ learning/graph_logistic_regression.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pylab as pl
+import csv
+
+in_file = "logistic_regression_results.csv"
+reader = csv.DictReader(open(in_file, 'r'), delimiter= ",")
+assert (reader.fieldnames == ["lead", "lag", "auc"])
+
+#initialize lags
+lags = {}
+for lag in range(1,15):
+	lags[lag] = {}
+
+for row in reader:
+	lags[int(row["lag"])][int(row["lead"])] = float(row["auc"])
+
+for lag in lags:
+	lead_aucs= lags[lag]
+	pl.plot(lead_aucs.keys(), lead_aucs.values(), label='lag = %s' % lag)
+
+# Plot AUC curve
+pl.ylim([0.0, 1.0])
+pl.xlabel('Lead')
+pl.ylabel('AUC of ROC')
+pl.title('Logistic Regression AUC as lead and lag vary')
+pl.legend(loc="lower center", ncol=3)
+pl.show()
