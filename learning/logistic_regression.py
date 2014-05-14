fixed log reg to use normalization; fixed all code to only predict through week 13

learning/logistic_regression.py
@@ -11,54 +11,57 @@ import os
 
 from sklearn import linear_model
 from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 import flatten_featureset
 
 def run_regression(train_file, test_file, lead, lag):
 	start_time = time.time()
-	intermediate_file = "data/tmp.csv"
+	intermediate_file1 = "data/train.csv"
+	intermediate_file2 = "data/test.csv"
 
-	flatten_featureset.create_features(intermediate_file, train_file, lead, lag)
-	train_data = np.genfromtxt(intermediate_file, delimiter = ',', skip_header = 1)
-	os.remove(intermediate_file)
+	flatten_featureset.create_features(intermediate_file1, train_file, lead, lag)
+	train_data = np.genfromtxt(intermediate_file1, delimiter = ',', skip_header = 1)
+	os.remove(intermediate_file1)
 
-	flatten_featureset.create_features(intermediate_file, test_file, lead, lag)
-	test_data = np.genfromtxt(intermediate_file, delimiter = ',', skip_header = 1)
-	os.remove(intermediate_file)
+	flatten_featureset.create_features(intermediate_file2, test_file, lead, lag)
+	test_data = np.genfromtxt(intermediate_file2, delimiter = ',', skip_header = 1)
+	os.remove(intermediate_file2)
 	
 	X_train = train_data[:,1:] #file format is [label list_of_features]
 	Y_train = train_data[:,0]
 	X_test = test_data[:,1:] #file format is [label list_of_features]
 	Y_test = test_data[:,0]
 
-	logreg = linear_model.LogisticRegression()
+	logreg = Pipeline([('scale', StandardScaler()), ('logreg', linear_model.LogisticRegression())])
 	logreg.fit(X_train, Y_train)
 
 	desired_label = 0 # want to predict if student will dropout
-	desired_label_index = logreg.classes_.tolist().index(desired_label) 
+	desired_label_index = logreg.steps[-1][1].classes_.tolist().index(desired_label) 
 
 	try:
 		predicted_probs = logreg.predict_proba(X_train)
 		fpr, tpr, thresholds = roc_curve(Y_train, predicted_probs[:, desired_label_index],  pos_label=desired_label)
 		roc_auc_train = auc(fpr, tpr)
 	except:
-		roc_auc_train = 0
+		roc_auc_train = 0.0
 
 	try:
 		predicted_probs = logreg.predict_proba(X_test)
 		fpr, tpr, thresholds = roc_curve(Y_test, predicted_probs[:, desired_label_index],  pos_label=desired_label)
 		roc_auc_test = auc(fpr, tpr)
 	except:
-		roc_auc_test = 0
+		roc_auc_test = 0.0
 
 	return (float(roc_auc_train), float(roc_auc_test))
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Create feature csv with given lead and lag.')
-	parser.add_argument('--train_file',type=str, default="data/features_forum_and_wiki_train.csv") # input csv
-	parser.add_argument('--test_file',type=str, default="data/features_forum_and_wiki_test.csv") # input csv
-	parser.add_argument('--lead',type=int, default=1)  # number of weeks ahead to predict
-	parser.add_argument('--lag',type=int, default=5)  # number of weeks of features to use
+	parser.add_argument('--train_file',type=str, default="data/features_no_collab_train.csv") # input csv
+	parser.add_argument('--test_file',type=str, default="data/features_no_collab_test.csv") # input csv
+	parser.add_argument('--lead',type=int, default=8)  # number of weeks ahead to predict
+	parser.add_argument('--lag',type=int, default=4)  # number of weeks of features to use
 	args = parser.parse_args()
 
 	train_auc, test_auc = run_regression(args.train_file, args.test_file, args.lead, args.lag)

learning/run_logistic_regression.py
@@ -18,7 +18,7 @@ def add_to_data(old_data, new_data):
 
 header = "lead,lag,auc"
 
-cohorts = ["forum_only", "wiki_only", "forum_and_wiki", "no_collab"]
+cohorts = ["wiki_only", "forum_and_wiki", "forum_only", "no_collab"]
 
 features_base = "features_"
 data_file_prefix = "data/" + features_base
@@ -35,8 +35,8 @@ for cohort in cohorts:
 
 	train_data = None
 	test_data = None
-	for lead in range (1,15):
-		for lag in range(1, 16 - lead):
+	for lead in range (1,14):
+		for lag in range(1, 15 - lead):
 			train_file = data_file_prefix + cohort + "_train" + data_file_suffix
 			test_file = data_file_prefix + cohort + "_test" + data_file_suffix
 			try:
@@ -48,6 +48,6 @@ for cohort in cohorts:
 	print "Ran logistic regression for %s in %s seconds" % (cohort, time.time() - start_time)
 
 	np.savetxt(train_results_file, np.atleast_2d(train_data), fmt="%s", delimiter=",", header= header, comments='')
-	graph_logistic_regression.graph_logreg(train_results_file, train_graph_file)
+	# graph_logistic_regression.graph_logreg(train_results_file, train_graph_file)
 	np.savetxt(test_results_file, np.atleast_2d(test_data), fmt="%s", delimiter=",", header= header, comments='')
-	graph_logistic_regression.graph_logreg(test_results_file, test_graph_file)
+	# graph_logistic_regression.graph_logreg(test_results_file, test_graph_file)

learning/run_randomized_logistic_regression.py
@@ -23,7 +23,8 @@ data_file_suffix = ".csv"
 results_prefix = "results/randomized_logistic_reg_"
 results_suffix = ".csv"
 
-results_file = results_prefix + features_base + results_suffix
+results_file = results_prefix + features_base + "averaged" + results_suffix
+raw_results_file = results_prefix + features_base + results_suffix
 
 header = "cohort,lead,lag," + ",".join(["feature_%s" % x for x in range(2,29)])
 
@@ -32,16 +33,19 @@ for cohort in cohorts:
 	data_file = data_file_prefix + features_base + cohort + data_file_suffix
 	total_weights = [0]*27
 	num_weights = 0
-	for lead in range (1,15):
-		for lag in range(1, 16 - lead):
-			try:
-				weights = run_regression(data_file, lead, lag)
-				averaged_weights = np.mean(np.reshape(weights, (-1, 27)), axis=0)
-				data = add_to_data(data, [cohort, lead, lag] + averaged_weights.tolist())
-				total_weights += averaged_weights
-				num_weights += 1
-			except Exception as e:
-				pass
+	with open(raw_results_file, "a") as myfile:
+		for lead in range (1,14):
+			for lag in range(1, 15 - lead):
+				try:
+					myfile.write("cohort: %s, lead: %s, lag: %s" % (cohort, lead, lag))
+					weights = run_regression(data_file, lead, lag)
+					myfile.write(weights)
+					averaged_weights = np.mean(np.reshape(weights, (-1, 27)), axis=0)
+					data = add_to_data(data, [cohort, lead, lag] + averaged_weights.tolist())
+					total_weights += averaged_weights
+					num_weights += 1
+				except Exception as e:
+					pass
 
 	average_weights = [weight / num_weights for weight in  total_weights]
 	data = add_to_data(data, [cohort, "-", "-"] + average_weights)